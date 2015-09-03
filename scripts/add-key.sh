76e34f8a706b77715fae147bb81725c9f7ee0a7a
keygx <y.kagiyama@gmail.com>
2015-09-04 00:23:56 +0900

    Update circle.yml

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -16,8 +16,8 @@ echo ${DISTRIBUTION_KEY_BASE64} | base64 -D > ${DIR}/dist.p12
 echo ${DISTRIBUTION_CERTIFICATE_BASE64} | base64 -D > ${DIR}/dist.cer
 security create-keychain -p ${KEYCHAIN_PASSWORD} ios-build.keychain
 security import ${DIR}/apple.cer -k ${KEYCHAIN_NAME} -T /usr/bin/codesign
-security import ${DIR}/dist.cer  -k ${KEYCHAIN_NAME} -T /usr/bin/codesign
-security import ${DIR}/dist.p12  -k ${KEYCHAIN_NAME} -T /usr/bin/codesign -P ${P12_PASSPHRASE}
+security import ${DIR}/dist.cer -k ${KEYCHAIN_NAME} -T /usr/bin/codesign
+security import ${DIR}/dist.p12 -k ${KEYCHAIN_NAME} -T /usr/bin/codesign -P ${P12_PASSPHRASE}
 security list-keychain -s ${KEYCHAIN_NAME}
 security unlock-keychain -p ${KEYCHAIN_PASSWORD} ${KEYCHAIN_NAME}
 rm -rf $DIR
