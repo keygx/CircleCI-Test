97783c43b56ef7fffeed422d5d8c2c94d836d72a
keygx <y.kagiyama@gmail.com>
2015-09-03 16:58:01 +0900

    Update circle.yml

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,4 +5,4 @@
 
 PROFILE_NAME=AdHocTest
 
-ios profiles:download ${PROFILE_NAME} --type distribution -u ${ITUNES_CONNECT_ID} -p ${ITUNES_CONNECT_PW}
+bundle exec ios profiles:download ${PROFILE_NAME} --type distribution -u ${ITUNES_CONNECT_ID} -p ${ITUNES_CONNECT_PW}
