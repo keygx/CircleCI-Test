cd1b4d0152ec84c618ab7d1c0a32f74d240a3062
keygx <y.kagiyama@gmail.com>
2015-09-04 14:37:07 +0900

    Update circle.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-ipa build --clean -s AdHoc
+# ipa build --clean -s AdHoc
+
+PROJECT_NAME='CircleCI-Test.xcodeproj'
+
+xcodebuild clean -project ${PROJECT_NAME}
+
+xcodebuild -project ${PROJECT_NAME} -sdk "iphoneos8.1" -scheme 'AdHoc' -configuration "AdHoc" -target "CircleCI-Test"
+
+xcrun -sdk "iphoneos8.1" PackageApplication "CircleCI-Test.app" -o "CircleCI-Test.ipa" -embed "~/Library/MobileDevice/Provisioning Profiles/AdHocTest.mobileprovision.mobileprovision"
+
+rm -rf build
