4b0e5fb5b7b092a100b4b04e6a112c7d7748feed
keygx <y.kagiyama@gmail.com>
2015-09-04 16:30:31 +0900

    Update circle.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-ipa build --clean -s AdHoc
+# ipa build --clean -s AdHoc
 
-# PROJECT_NAME='CircleCI-Test.xcodeproj'
-#
-# xcodebuild clean -project ${PROJECT_NAME}
-#
-# xcodebuild -project ${PROJECT_NAME} -destination 'name=iPhone 6' -scheme 'AdHoc' -configuration 'AdHoc' install DSTROOT='.'
-#
-# xcrun -sdk 'iphoneos8.4' PackageApplication 'Applications/CircleCI-Test.app' -o 'CircleCI-Test.ipa' -embed '~/Library/MobileDevice/Provisioning Profiles/AdHocTest.mobileprovision.mobileprovision'
+PROJECT_NAME=CircleCI-Test
+SCHEME_NAME=AdHoc
+CONFIGURATION_NAME=AdHoc
+MOBILEPROVISION_NAME=AdHocTest
+
+xcodebuild clean -project ${PROJECT_NAME}
+xcodebuild -project "${PROJECT_NAME}.xcodeproj" -destination 'name=iPhone 6' -scheme ${SCHEME_NAME} -configuration ${CONFIGURATION_NAME} install DSTROOT='.'
+xcrun -sdk 'iphoneos' PackageApplication "${PWD}/Applications/${PROJECT_NAME}.app" -o "${PWD}/CircleCI-Test.ipa" -embed "~/Library/MobileDevice/Provisioning Profiles/${MOBILEPROVISION_NAME}.mobileprovision"
+rm -rf "${PWD}/Applications"
