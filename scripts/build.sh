462728f5e48a9f916a723ec96ba205922108d760
keygx <y.kagiyama@gmail.com>
2015-09-04 17:51:39 +0900

    Update circle.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# ipa build --clean -s AdHoc
+ipa build --clean -s AdHoc
 
-PROJECT_NAME=CircleCI-Test
-SCHEME_NAME=AdHoc
-MOBILEPROVISION_NAME=AdHocTest
-
-xcodebuild clean -project ${PROJECT_NAME}
-xcodebuild -project "${PROJECT_NAME}.xcodeproj" -sdk 'iphoneos' -target ${PROJECT_NAME} -configuration ${PROJECT_NAME} install DSTROOT="${PWD}"
-xcrun -sdk 'iphoneos' PackageApplication "${PWD}/Applications/${PROJECT_NAME}.app" -o "${PWD}/CircleCI-Test.ipa" -embed "~/Library/MobileDevice/Provisioning Profiles/${MOBILEPROVISION_NAME}.mobileprovision"
-rm -rf "${PWD}/Applications"
-rm -rf "${PWD}/build"
+# PROJECT_NAME=CircleCI-Test
+# SCHEME_NAME=AdHoc
+# MOBILEPROVISION_NAME=AdHocTest
+#
+# xcodebuild clean -project ${PROJECT_NAME}
+# xcodebuild -project "${PROJECT_NAME}.xcodeproj" -sdk 'iphoneos' -target ${PROJECT_NAME} -configuration ${PROJECT_NAME} install DSTROOT="${PWD}"
+# xcrun -sdk 'iphoneos' PackageApplication "${PWD}/Applications/${PROJECT_NAME}.app" -o "${PWD}/CircleCI-Test.ipa" -embed "~/Library/MobileDevice/Provisioning Profiles/${MOBILEPROVISION_NAME}.mobileprovision"
+# rm -rf "${PWD}/Applications"
+# rm -rf "${PWD}/build"
 
  # -target ${PROJECT_NAME}
  # -configuration ${PROJECT_NAME}
--- scripts/deploygate.sh
+++ scripts/deploygate.sh
@@ -3,6 +3,6 @@
 # DEPLOYGATE_API_TOKEN
 # DEPLOYGATE_USERNAME
 
-# ipa distribute:deploygate -m --api_token ${DEPLOYGATE_API_TOKEN} --user_name ${DEPLOYGATE_USERNAME}
+ipa distribute:deploygate -m --api_token ${DEPLOYGATE_API_TOKEN} --user_name ${DEPLOYGATE_USERNAME}
 
-dgate push keygx "${PWD}/CircleCI-Test.ipa"
+# dgate push keygx "${PWD}/CircleCI-Test.ipa"
