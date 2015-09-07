e1421cf6b02a666b9edb8a16761b70f0441a02f0
keygx <y.kagiyama@gmail.com>
2015-09-07 20:50:11 +0900

    Change circle.yml

--- scripts/deploygate.sh
+++ scripts/deploygate.sh
@@ -3,6 +3,9 @@
 # DEPLOYGATE_API_TOKEN
 # DEPLOYGATE_USERNAME
 
-ipa distribute:deploygate -m --api_token ${DEPLOYGATE_API_TOKEN} --user_name ${DEPLOYGATE_USERNAME}
+curl -F file=@${PWD}/CircleCI-Test.ipa -F token=97b0b37e3a7267c25220ba4a29ba5d629525a245 https://deploygate.com/api/users/keygx/apps
+
+
 
 # dgate push keygx "${PWD}/CircleCI-Test.ipa"
+# ipa distribute:deploygate -m --api_token ${DEPLOYGATE_API_TOKEN} --user_name ${DEPLOYGATE_USERNAME}
