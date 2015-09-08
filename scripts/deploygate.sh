d171e90c6dff65255a49f3c5d1bc8fed5b9da81d
keygx <y.kagiyama@gmail.com>
2015-09-08 13:44:45 +0900

    Change circle.yml

--- scripts/deploygate.sh
+++ scripts/deploygate.sh
@@ -2,4 +2,4 @@
 
 DIR=$PWD/certs
 
-curl -F file=@${DIR}/CircleCI-Test.ipa -F token=97b0b37e3a7267c25220ba4a29ba5d629525a245 https://deploygate.com/api/users/keygx/apps
+curl -F "file=@${DIR}/CircleCI-Test.ipa" -F "token=97b0b37e3a7267c25220ba4a29ba5d629525a245" https://deploygate.com/api/users/keygx/apps
