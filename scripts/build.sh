09af27ca0868b2386ad03f2b3d0744630e283c1f
keygx <y.kagiyama@gmail.com>
2015-09-08 14:09:19 +0900

    Change circle.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,8 @@ PROJECT_NAME=CircleCI-Test
 SCHEME_NAME=AdHoc
 MOBILEPROVISION_NAME=AdHocTest
 
-xcodebuild clean -project ${PROJECT_NAME} -scheme AdHoc
-xcodebuild -project "${PROJECT_NAME}.xcodeproj" -sdk "iphoneos" -configuration "${SCHEME_NAME}" -target ${PROJECT_NAME} install DSTROOT="${DIR}"
+xcodebuild clean -project "${PROJECT_NAME}.xcodeproj" -scheme "${SCHEME_NAME}"
+xcodebuild -project "${PROJECT_NAME}.xcodeproj" -sdk "iphoneos" -scheme "${SCHEME_NAME}" install DSTROOT="${DIR}"
 xcrun -sdk 'iphoneos' PackageApplication "${DIR}/Applications/${PROJECT_NAME}.app" -o "${DIR}/CircleCI-Test.ipa" -embed "~/${MOBILEPROVISION_NAME}.mobileprovision"
 rm -rf "${DIR}/Applications"
 rm -rf "${DIR}/build"
