e3d671cc922ceaae6b6e01b5c341ae14177571d2
keygx <y.kagiyama@gmail.com>
2015-09-08 15:51:15 +0900

    Change circle.yml

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -3,6 +3,9 @@
 # ITUNES_CONNECT_ID
 # ITUNES_CONNECT_PW
 
+UUID=58a4e3e2-05d2-4a02-9cc7-be6f4116503a
 PROFILE_NAME=AdHocTest
 
 ios profiles:download ${PROFILE_NAME} --type distribution -u ${ITUNES_CONNECT_ID} -p ${ITUNES_CONNECT_PW}
+
+mv $HOME/${PROFILE_NAME}.mobileprovision $HOME/Library/MobileDevice/Provisioning\ Profiles/${UUID}.mobileprovision
