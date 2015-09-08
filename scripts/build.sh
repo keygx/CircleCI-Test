86bbc1da659c5eb4809ec258a6c7e313752e9050
keygx <y.kagiyama@gmail.com>
2015-09-08 14:03:10 +0900

    Change circle.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
 DIR=$PWD/certs
-
-#ipa build --clean -s AdHoc
-
-# PROJECT_NAME=CircleCI-Test
-# SCHEME_NAME=AdHoc
-# MOBILEPROVISION_NAME=AdHocTest
+PROJECT_NAME=CircleCI-Test
+SCHEME_NAME=AdHoc
+MOBILEPROVISION_NAME=AdHocTest
 
 xcodebuild clean -project ${PROJECT_NAME} -scheme AdHoc
-xcodebuild -project "${PROJECT_NAME}.xcodeproj" -sdk "iphoneos" -configuration "AdHoc" -target ${PROJECT_NAME} install DSTROOT="${DIR}"
-xcrun -sdk 'iphoneos' PackageApplication "${DIR}/Applications/${PROJECT_NAME}.app" -o "${DIR}/CircleCI-Test.ipa" -embed "~/AdHocTest.mobileprovision"
+xcodebuild -project "${PROJECT_NAME}.xcodeproj" -sdk "iphoneos" -configuration "${SCHEME_NAME}" -target ${PROJECT_NAME} install DSTROOT="${DIR}"
+xcrun -sdk 'iphoneos' PackageApplication "${DIR}/Applications/${PROJECT_NAME}.app" -o "${DIR}/CircleCI-Test.ipa" -embed "~/${MOBILEPROVISION_NAME}.mobileprovision"
 rm -rf "${DIR}/Applications"
 rm -rf "${DIR}/build"
 
--- scripts/deploygate.sh
+++ scripts/deploygate.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# DEPLOYGATE_TOKEN
+
 DIR=$PWD/certs
 
-curl -F "file=@${DIR}/CircleCI-Test.ipa" -F "token=97b0b37e3a7267c25220ba4a29ba5d629525a245" https://deploygate.com/api/users/keygx/apps
+curl -F "file=@${DIR}/CircleCI-Test.ipa" -F "token=${DEPLOYGATE_TOKEN}" https://deploygate.com/api/users/keygx/apps
